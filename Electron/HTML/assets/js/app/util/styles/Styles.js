4a99b3f18b5d007b11b90486759d121d033d1aee
jiaolyulu <ll4132@nyu.edu>
2023-04-04 15:14:02 -0400

    fix: change every background

--- Electron/HTML/assets/js/app/util/styles/Styles.js
+++ Electron/HTML/assets/js/app/util/styles/Styles.js
@@ -31,7 +31,7 @@ Class(function Styles() {
         emperor: '#4F4F4F',
         mineShaft: '#333333',
         // silver: '#BDBDBD',
-        colorWhite:"#fff8e9",
+        colorWhite:"#FFFAF1",
         concrete: '#E0E0E0',
         royalBlue: '#1A73E8',
         malibu: '#4DC3FF',
