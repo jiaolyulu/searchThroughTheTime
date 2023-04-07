ab52445c02b3859ca92beb30aa0720876d021015
jiaolyulu <ll4132@nyu.edu>
2023-04-07 15:21:15 -0400

    feat: change layout of the touchscreen according to Matt Figma

--- Electron/HTML/assets/js/app/layouts/time/desktop/TimeDesktopDetailView.js
+++ Electron/HTML/assets/js/app/layouts/time/desktop/TimeDesktopDetailView.js
@@ -5,18 +5,27 @@ Class(function TimeDesktopDetailView() {
     const _this = this;
     const $this = _this.element;
     let $TDDContainer, $TDDContentContainer, $TDDContent;
-    const buttonWidth = 1200;
+    const buttonWidth = 600;
     const buttonHeight = 140;
-
+    const textWidth = 900;
+    let margin = 500;
 
     initHTML();
     addListeners();
 
     function initHTML() {
         $TDDContainer = $this.create('detail-container');
-        $TDDContainer.css({ "transform-origin": "top left", "display": "none", "width": "1920px", "height": "720px", "transform": "rotate(90deg) translate(0,-720px)", "position": "relative !important", "display": "flex", "justify-content": "center", "align-items": "center" });
+        $TDDContainer.css({ "transform-origin": "top left", "display": "none", "width": "1920px", "height": "720px", "transform": "rotate(90deg) translate(0,-720px)", "position": "relative !important", "align-items": "center" });
+        $TDDTitle = $TDDContainer.create('detail-content-container');
+        $TDDTitle.css({ "width": `${textWidth}px`, "height": `${buttonHeight - 20}px`, "position": "absolute", "transform": "translate(-50%,0)", "left": `${textWidth / 2 +margin}px`});
+        $TDDTitleSub = $TDDTitle.create('title-sub');
+        $TDDTitleSub.css({ "font-size": "2rem", "color": "#808080", "top": "0px" });
+        $TDDTitleSub.div.innerHTML = 'Exploring';
+        $TDDTitleMain = $TDDTitle.create('title-main');
+        $TDDTitleMain.css({ "font-size": "3rem", "color": "black", "bottom": "0px" });
+        $TDDTitleMain.div.innerHTML = 'Exploring';
         $TDDContentContainer = $TDDContainer.create('detail-content-container');
-        $TDDContentContainer.css({ "background-color": "#4285F4", "width": `${buttonWidth}px`, "height": `${buttonHeight}px`, "border-radius": `${buttonHeight}px`, "position": "relative !important", "display": "flex", "justify-content": "center", "align-items": "center" });
+        $TDDContentContainer.css({ "background-color": "#4285F4", "width": `${buttonWidth}px`, "height": `${buttonHeight}px`, "border-radius": `${buttonHeight}px`, "position": "relative !important", "display": "flex", "justify-content": "center", "align-items": "center", "right": `${margin}px` });
         $TDDContent = $TDDContentContainer.create('detail-content', 'span');
         $TDDContent.text("default");
         console.log(MainStore.get("selectedMilestone"));
@@ -48,6 +57,11 @@ Class(function TimeDesktopDetailView() {
         words.forEach(element => {
             stringCombined += " " + element.charAt(0).toUpperCase() + element.slice(1);
         });
+
+        //28:=> 300 margin, 5=> 500, so it is roughly 200 to 20 580-10*length
+        margin = 580 - 10 * stringCombined.length;
+        $TDDContentContainer.css({ "right": `${margin}px` });
+        $TDDTitle.css({"left": `${textWidth / 2 + margin}px`});
         return stringCombined;
     }
 
@@ -55,12 +69,13 @@ Class(function TimeDesktopDetailView() {
         const view = GlobalStore.get('view');
         if (view === 'DetailView' & $TDDContainer.div.style.display === "none") {
             console.log("now in detail view");
-            $TDDContainer.css({ "display": "flex" });
+            $TDDContainer.css({ "display": "grid" });
             console.log(MainStore.get("selectedMilestone"));
-            $TDDContent.text(`Exploring${dealWithString(MainStore.get("selectedMilestone"))}`);
+            $TDDTitleMain.div.innerHTML = dealWithString(MainStore.get("selectedMilestone"));
+            $TDDContent.text(`Back to Timeline`);
             startIntroAnimation();
         } else {
-            if (view !== 'DetailView' & $TDDContainer.div.style.display === "flex") {
+            if (view !== 'DetailView' & $TDDContainer.div.style.display === "grid") {
                 startExitAnimation();
             }
         }
