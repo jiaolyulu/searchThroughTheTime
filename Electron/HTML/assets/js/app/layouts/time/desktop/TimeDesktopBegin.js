bddd415eda13ddb07c5f877dba4b84dffc1bfc0c
Matt Franklin <mfranklin@deeplocal.com>
2023-04-18 14:48:49 -0400

    scroll to explore

--- Electron/HTML/assets/js/app/layouts/time/desktop/TimeDesktopBegin.js
+++ Electron/HTML/assets/js/app/layouts/time/desktop/TimeDesktopBegin.js
@@ -14,24 +14,24 @@ Class(function TimeDesktopBegin() {
 
     function initHTML() {
         $beginContainer = $this.create('begin-container');
-        $beginContainer.css({ "transform-origin": "top left", "width": "1920px", "height": "720px", "transform": "rotate(90deg) translate(0,-720px)", position: "relative !important", display: "flex", "justify-content": "center" , "align-items": "center" });
+        $beginContainer.css({ "transform-origin": "top left", "width": "1920px", "height": "720px", "transform": "rotate(90deg) translate(0,-720px)", position: "relative !important", display: "flex", "justify-content": "center", "align-items": "center" });
         $beginButtonContainer = $beginContainer.create('begin-button-container');
-        $beginButtonContainer.css({backgroundColor: "#4285F4", width: `${buttonWidth}px`, height: `${buttonHeight}px`, borderRadius: `${buttonHeight}px`, position: "relative !important", display: "flex", "justify-content": "center" , "align-items": "center" });
-        $beginButtonContent = $beginButtonContainer.create('begin-button-content', 'span').text("Scroll to Begin");
+        $beginButtonContainer.css({ backgroundColor: "#4285F4", width: `${buttonWidth}px`, height: `${buttonHeight}px`, borderRadius: `${buttonHeight}px`, position: "relative !important", display: "flex", "justify-content": "center", "align-items": "center" });
+        $beginButtonContent = $beginButtonContainer.create('begin-button-content', 'span').text("Scroll to explore");
         $beginButtonContent.css({ color: "white", fontSize: "40px", position: "absolute", left: "50%", top: "50%", transform: "translate(-50%,-50%)" });
     }
 
     function addListeners() {
-        
+
         const interaction = _this.initClass(Interaction, $beginButtonContainer);
         interaction.ignoreLeave = true;
 
         //_this.events.sub(interaction, Interaction.END, onClick);
-        $beginButtonContainer.div.addEventListener('touchend',onClick, false);
+        $beginButtonContainer.div.addEventListener('touchend', onClick, false);
         $beginButtonContainer.interact(false, onClick); //however this only work with mouse not touchscreen touch
     }
 
-    
+
     async function onClick() {
         leave();
         //await $this.wait(500);
@@ -42,7 +42,7 @@ Class(function TimeDesktopBegin() {
 
     function leave() {
         _this.clearTimers();
-        $beginButtonContainer.tween({ width: 1800,opacity: 0}, 1300, 'easeOutCubic');
+        $beginButtonContainer.tween({ width: 1800, opacity: 0 }, 1300, 'easeOutCubic');
 
     }
 
