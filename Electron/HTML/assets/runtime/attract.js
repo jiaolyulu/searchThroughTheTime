ca93cdce1cbd16278ae29d70bdbfa811f23cbe91
jiaolyulu <ll4132@nyu.edu>
2023-09-01 14:58:18 -0400

    fix: tab width to 4

--- Electron/HTML/assets/runtime/attract.js
+++ Electron/HTML/assets/runtime/attract.js
@@ -7,13 +7,13 @@
  */
 
 /**Variables */
-let activityTimer = 60; 
+let activityTimer = 60;
 // set interval to check state and start attract loop
 
 // state management
 let userHasScrolled = false;
 let userHasClicked = false;
-let interationState =  null;
+let interationState = null;
 let attractLoopVisible = false;
 let ignoreUserInput = false; // flag to ignore user input during into/exit attract loop animations
 
@@ -21,27 +21,27 @@ let ignoreUserInput = false; // flag to ignore user input during into/exit attra
 let attractEvent; // fired to alert other components to experience state (used for disabling timeline scrolling, etc)
 
 const fireStartInteractionMsg = () => {
-  if (interationState!=="started") {
-    interationState = "started";
-    let startInteractionEvent = new CustomEvent('START_INTERACTION', {  });
-    window.dispatchEvent(startInteractionEvent);
-  }
-}
+    if (interationState !== "started") {
+        interationState = "started";
+        let startInteractionEvent = new CustomEvent("START_INTERACTION", {});
+        window.dispatchEvent(startInteractionEvent);
+    }
+};
 const fireEndInteractionMsg = () => {
-  if (interationState==="started") {
-    interationState = "ended";
-    let endInteractionEvent = new CustomEvent('END_INTERACTION', {  });
-    window.dispatchEvent(endInteractionEvent);
-  }
-}
+    if (interationState === "started") {
+        interationState = "ended";
+        let endInteractionEvent = new CustomEvent("END_INTERACTION", {});
+        window.dispatchEvent(endInteractionEvent);
+    }
+};
 
 window.onwheel = function (e) {
     //console.log('ATTRACT: USER SCROLLED');
-  
+
     fireStartInteractionMsg();
-    
+
     userHasScrolled = true;
-    
+
     if (attractLoopVisible && !ignoreUserInput) {
         ignoreUserInput = true;
         startExitAnimation();
@@ -51,32 +51,35 @@ window.onclick = function (e) {
     //console.log('ATTRACT: USER CLICKED');
     fireStartInteractionMsg();
     userHasClicked = true;
-    
+
     if (attractLoopVisible && !ignoreUserInput) {
         ignoreUserInput = true;
         startExitAnimation();
     }
 };
 
-
-window.addEventListener('GB_STATE_CHANGED', (e) => {
-  console.log('GB_STATE_CHANGED',e)
-  activityTimer = e.detail.attractTimer
-  clearInterval(userTimeout);
-  restartActivityTimer()
-})
+window.addEventListener("GB_STATE_CHANGED", (e) => {
+    console.log("GB_STATE_CHANGED", e);
+    activityTimer = e.detail.attractTimer;
+    clearInterval(userTimeout);
+    restartActivityTimer();
+});
 
 /**State Machine & Interval Logic */
 
 // function to be called by setInterval (this runs during timeline view and tests to engage attract loop)
 const checkForInactivity = () => {
-    console.log(`TIMEOUT: ATTRACT: userhasclicked:${userHasClicked}, userhasscrolled:${userHasScrolled}, attractloopvisible:${attractLoopVisible} `);
-    if (!attractLoopVisible && (!userHasClicked && !userHasScrolled)) {
+    console.log(
+        `TIMEOUT: ATTRACT: userhasclicked:${userHasClicked}, userhasscrolled:${userHasScrolled}, attractloopvisible:${attractLoopVisible} `
+    );
+    if (!attractLoopVisible && !userHasClicked && !userHasScrolled) {
         // if user hasn't engaged the main timeline screen for {activityTimer} time, turn on the attract loop
-      console.log(`ATTRACT: No user activity in ${activityTimer} seconds reported on timeline view`);
+        console.log(
+            `ATTRACT: No user activity in ${activityTimer} seconds reported on timeline view`
+        );
         // start attract loop animation
         startIntroAnimation();
-        gsap.timeline().to(document.getElementById('Stage'), {
+        gsap.timeline().to(document.getElementById("Stage"), {
             opacity: 0,
             duration: 1.5,
             onComplete: () => {
@@ -88,10 +91,10 @@ const checkForInactivity = () => {
                 clearInterval(userTimeout);
                 // update state to allow user clicks to be recorded again (turned off during attract loop exit animation execution)
                 ignoreUserInput = false;
-            }
+            },
         });
     } else {
-        console.log('ATTRACT: no state change required');
+        console.log("ATTRACT: no state change required");
         resetUserInteractedStates();
     }
 };
@@ -99,25 +102,25 @@ const checkForInactivity = () => {
 let userTimeout = setInterval(checkForInactivity, activityTimer * 1000);
 
 const restartActivityTimer = () => {
-  userTimeout = setInterval(checkForInactivity, activityTimer * 1000);
-}
+    userTimeout = setInterval(checkForInactivity, activityTimer * 1000);
+};
 
 // function to toggle the attract loop and clean up the state machine
 function toggleAttractLoop() {
-    console.log('ATTRACT: toggleAttractLoop');
+    console.log("ATTRACT: toggleAttractLoop");
     // toggle the attract loop div
-    const el = document.getElementsByClassName('attractLoopScr');
+    const el = document.getElementsByClassName("attractLoopScr");
     const stage = document.getElementById("Stage");
     if (attractLoopVisible) {
-        el[0].style.display = 'none';
-        stage.style.display = 'flex';
-        let unmountEvent = new CustomEvent('UNMOUNT');
+        el[0].style.display = "none";
+        stage.style.display = "flex";
+        let unmountEvent = new CustomEvent("UNMOUNT");
         window.dispatchEvent(unmountEvent);
     } else {
-        el[0].style.display = 'flex';
-        let renderEvent = new CustomEvent('RENDER');
+        el[0].style.display = "flex";
+        let renderEvent = new CustomEvent("RENDER");
         window.dispatchEvent(renderEvent);
-        stage.style.display = 'none';
+        stage.style.display = "none";
     }
     resetUserInteractedStates();
     attractLoopVisible = !attractLoopVisible;
@@ -126,20 +129,21 @@ function toggleAttractLoop() {
 
 // helper function to clean up user state excluding the attractLoopVisible flag
 function resetUserInteractedStates() {
-    console.log('ATTRACT: cleanup user state');
+    console.log("ATTRACT: cleanup user state");
     // user interaction state management
     if (userHasClicked) userHasClicked = !userHasClicked;
     if (userHasScrolled) userHasScrolled = !userHasScrolled;
 }
 
 function fireAttractLoopMsg() {
-    
-    attractEvent = new CustomEvent('ATTRACT_ENABLED', { detail: attractLoopVisible });
+    attractEvent = new CustomEvent("ATTRACT_ENABLED", {
+        detail: attractLoopVisible,
+    });
     window.dispatchEvent(attractEvent);
 }
 
 function fireIntroAnimationMsg() {
-    let introEvent = new CustomEvent('INTRO_ANIMATION');
+    let introEvent = new CustomEvent("INTRO_ANIMATION");
     window.dispatchEvent(introEvent);
 }
 
@@ -148,31 +152,37 @@ function fireIntroAnimationMsg() {
 // function to start the attract animation
 function startIntroAnimation() {
     fireEndInteractionMsg();
-  
-  
-    portalTween = gsap.timeline()
-        .to(document.getElementsByClassName('attractLoopScr'), { opacity: 1, duration: 1.5 });
+
+    portalTween = gsap
+        .timeline()
+        .to(document.getElementsByClassName("attractLoopScr"), {
+            opacity: 1,
+            duration: 1.5,
+        });
 }
 
 // function to start the exit attract loop animation
 function startExitAnimation() {
-    console.log('ATTRACT: starting startExitAnimation');
-    portalTween = gsap.timeline(
-        {
+    console.log("ATTRACT: starting startExitAnimation");
+    portalTween = gsap
+        .timeline({
             onStart: () => {
-                let exitEvent = new CustomEvent('EXIT_ANIMATION');
+                let exitEvent = new CustomEvent("EXIT_ANIMATION");
                 window.dispatchEvent(exitEvent);
-            }
-        }
-    )
-        .to(document.getElementsByClassName('attractLoopScr'), {
-            opacity: 0,
-            duration: 2,
-            onComplete: startExitAnimationHelper
-        }, 1)
-        .to(document.getElementById('Stage'), {
+            },
+        })
+        .to(
+            document.getElementsByClassName("attractLoopScr"),
+            {
+                opacity: 0,
+                duration: 2,
+                onComplete: startExitAnimationHelper,
+            },
+            1
+        )
+        .to(document.getElementById("Stage"), {
             opacity: 1,
-            duration: 2
+            duration: 2,
         });
 }
 // helper function for exit animation's onComplete callback
--- Electron/HTML/assets/runtime/websocket.js
+++ Electron/HTML/assets/runtime/websocket.js
@@ -1,55 +1,58 @@
 const { Gumband } = require("@deeplocal/gumband-node-sdk");
 
-
 const socket = new WebSocket("ws://localhost:8080/state");
 const consoleService = (...args) => {
-  let result = '';
-  for (let i = 0; i < args.length; i++) {
-    result += args[i]+' ';
-  }
-  console.log(result);
-  let websocketMsg=JSON.stringify({"console":  result })
-  socket.send(websocketMsg)
-}
+    let result = "";
+    for (let i = 0; i < args.length; i++) {
+        result += args[i] + " ";
+    }
+    console.log(result);
+    let websocketMsg = JSON.stringify({ console: result });
+    socket.send(websocketMsg);
+};
 socket.addEventListener("open", (event) => {
-  consoleService('socket connected');
+    consoleService("socket connected");
 });
 
 socket.addEventListener("message", (event) => {
-  const data = JSON.parse(event.data);
-  if (data.state?.attractTime) {
-    consoleService("AttractTime is", data.state.attractTime);
-    let curAttractTime = data.state.attractTime;
-    let StateChangeFromGumBand = new CustomEvent("GB_STATE_CHANGED" ,{
-      detail:{ attractTimer: curAttractTime },
-    });
-    window.dispatchEvent(StateChangeFromGumBand);
-  }
+    const data = JSON.parse(event.data);
+    if (data.state?.attractTime) {
+        consoleService("AttractTime is", data.state.attractTime);
+        let curAttractTime = data.state.attractTime;
+        let StateChangeFromGumBand = new CustomEvent("GB_STATE_CHANGED", {
+            detail: { attractTimer: curAttractTime },
+        });
+        window.dispatchEvent(StateChangeFromGumBand);
+    }
 });
 
 socket.addEventListener("close", (event) => {
-  consoleService("WebSocket connection closed");
+    consoleService("WebSocket connection closed");
 });
-
 
 //!! ====================================================== //
 //!! =================== Gumband Metrics ================== //
 //!! ====================================================== //
-window.addEventListener('END_INTERACTION', () => {
-  let websocketMsg = JSON.stringify({ "metrics": { eventName: 'EndInteraction', value: {} } })
-  socket.send(websocketMsg)
-})
+window.addEventListener("END_INTERACTION", () => {
+    let websocketMsg = JSON.stringify({
+        metrics: { eventName: "EndInteraction", value: {} },
+    });
+    socket.send(websocketMsg);
+});
 
-window.addEventListener('START_INTERACTION', () => {
-  let websocketMsg = JSON.stringify({ "metrics": { eventName: 'StartInteraction', value: {} } })
-  socket.send(websocketMsg)
-})
+window.addEventListener("START_INTERACTION", () => {
+    let websocketMsg = JSON.stringify({
+        metrics: { eventName: "StartInteraction", value: {} },
+    });
+    socket.send(websocketMsg);
+});
 
-window.addEventListener('CARD_EVENT', (payload) => {
-  const { card_name, action } = payload.detail;
-  console.log(payload, card_name,action);
-  let currentEventName = card_name
-  let websocketMsg = JSON.stringify({ "metrics": { eventName: currentEventName, value: {action} } })
-  socket.send(websocketMsg)
-  
-})
+window.addEventListener("CARD_EVENT", (payload) => {
+    const { card_name, action } = payload.detail;
+    console.log(payload, card_name, action);
+    let currentEventName = card_name;
+    let websocketMsg = JSON.stringify({
+        metrics: { eventName: currentEventName, value: { action } },
+    });
+    socket.send(websocketMsg);
+});
